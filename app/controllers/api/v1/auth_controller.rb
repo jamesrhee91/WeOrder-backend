63009772e5d5054df9cb2d03106eabcb0db90825
jamesrhee91 <jamesshrhee@gmail.com>
2017-09-12 18:12:01 -0400

    render error message for auth

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -3,12 +3,14 @@ class Api::V1::AuthController < ApplicationController
   # method for logging in --- authenticate user and send him back with token
   def create
     user = User.find_by(first_name: params[:name])
+    # byebug
     if user && user.authenticate(params[:password])
       payload = { user_id: user.id}
       token = issue_token(payload)
       render json: {user: user, jwt: token}
       # a user exists and I can authenticate a user so successs
     else
+      render json: {error: "Not Authenticated"}
     end
   end
 end
