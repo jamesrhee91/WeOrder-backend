41b12f1b20c59a6e3b5968146c109a7f6c13ebc2
yakovkiff <yakov.kiffel@gmail.com>
2017-09-12 10:48:36 -0400

    can save order data to database

--- app/controllers/api/v1/user_restaurants_controller.rb
+++ app/controllers/api/v1/user_restaurants_controller.rb
@@ -3,23 +3,24 @@
 class Api::V1::UserRestaurantsController < ApplicationController
 
   def create
-    restaurant = Restaurant.create(restaurant_params)
-    byebug
-    # restaurant.update(restaurant_params)
-    users_params.each { |user_params|
-      user = User.find_or_create_by(user_params)
-      UserRestaurant.create(user, restaurant)
+
+    restaurant = Restaurant.find_by(yelp_id: params[:restaurant][:yelpId])
+    if !restaurant
+      restaurant = Restaurant.create(yelp_id: params[:restaurant][:yelpId], name: params[:restaurant][:name], address: params[:restaurant][:address])
+    end
+
+    params[:users].each { |user_hash|
+      user = User.find_by(first_name: user_hash[:name])
+      if !user
+        user = User.create(first_name: user_hash[:name], password: 'password')
+      end
+      user_restaurant = UserRestaurant.create(user_id: user.id, restaurant_id: restaurant.id)
+      # user.user_restaurants << user_restaurant
+      # restaurant.user_restaurants << user_restaurant
+      byebug
     }
   end
 
-  private
 
-  def users_params
-    params.require(:users)
-  end
-
-  def restaurant_params
-    params.require(:restaurant).permit(:name, :address)
-  end
 
 end
